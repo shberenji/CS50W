55fca75175845218e939b4b0d8ff32e50ff4d05f
Shahram Berenji <shberenji@users.noreply.github.com>
2022-11-15 22:46:24 +0100

    Add files via upload
    Add Users madule

--- airline/flights/urls.py
+++ airline/flights/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index.html")
+    path("", views.index, name="index.html"),
+    path("<int:flight_id>", views.flight, name="flight"),
+    path("<int:flight_id>/book", views.book, name="book")
 ]
